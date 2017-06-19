107d3b5b4f6454379e0dca46a599f18bfe20a79c
sahmed07 <sahmed07@D-2180.i2c.com>
2017-06-19 16:22:09 +0500

    DataFetch threads added ...

--- src/com/batchproject/Main.java
+++ src/com/batchproject/Main.java
@@ -3,24 +3,28 @@ package com.batchproject;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 import com.batchproject.config.Config;
 import com.batchproject.datawrite.DataWrite;
+import com.batchproject.dbconnection.DBConnection;
+import com.batchproject.fetch.DataFetch;
 
 public class Main {
-	//TODO blocking queue for results sets initialization
+	// TODO blocking queue for results sets initialization
 	static int batchSize;
 	static int threadPoolSize;
 	static int totalBatches;
 	static PrintWriter writer;
+	static String sql = "select trace_audit_no, card_prg_id, trans_date from trans_requests where trace_audit_no >= {} and trace_audit_no <= {};";
 	static String outputFileName = "output1.csv";
-	private static BlockingQueue<ResultSet> resultSets = new LinkedBlockingQueue<ResultSet>();	 
+	private static BlockingQueue<ResultSet> resultSets = new LinkedBlockingQueue<ResultSet>();
+
 	public static void main(String[] args) {
 		try {
 			writer = new PrintWriter(new File(outputFileName));
@@ -28,32 +32,32 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	
+
 		batchSize = Config.getIntValue("batchsize");
 		threadPoolSize = Config.getIntValue("threadpoolsize");
-		
+
 		int lower = Config.getIntValue("lower");
 		int upper = Config.getIntValue("upper");
-		
-		totalBatches = (int) Math.ceil((double)(upper - lower + 1)/batchSize);	
+
+		totalBatches = (int) Math.ceil((double) (upper - lower + 1) / batchSize);
 		System.out.println(threadPoolSize);
 		// TODO Auto-generated method stub
-		
-		
-		//TODO Initiate writedata threads
+
+		// TODO Initiate writedata and fetchthreads size;
 		new Thread() {
 			public void run() {
-				ExecutorService writerExecutor = Executors.newFixedThreadPool(threadPoolSize);
-				 for (int i = 0; i < totalBatches; ++i) {
-					 writerExecutor.execute(new DataWrite(resultSets,writer));
-				 }
-				writerExecutor.shutdown();
-				while (!writerExecutor.isTerminated()) {
+				ExecutorService executor = Executors.newFixedThreadPool(threadPoolSize);
+				for (int i = 0; i < totalBatches; ++i) {
+					Connection con = DBConnection.getConnection();
+					executor.execute(new DataFetch(con,resultSets,sql,lower,upper));
+					executor.execute(new DataWrite(resultSets, writer));
+					
+				}
+				executor.shutdown();
+				while (!executor.isTerminated()) {
 				}
 			}
 		}.start();
 
-		
 	}
 }
--- src/com/batchproject/dbconnection/DBConnection.java
+++ src/com/batchproject/dbconnection/DBConnection.java
@@ -1,6 +1,10 @@
 package com.batchproject.dbconnection;
 
-import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
 
 import com.batchproject.config.Config;
 
@@ -10,28 +14,40 @@ public final class DBConnection {
 	//TODO make connection string generic, make constructure
 	private static final String DB_URL;
 	private static DataSource datasource;
+	private static Connection con = null;
 	
 	
 	
 	static {
 		JDBC_DRIVER = Config.getStringValue("driver");
 		StringBuilder url = new StringBuilder(Config.getStringValue("url"));
-		url.append("user=" + Config.getStringValue("username") + ";");
-		url.append("password" + Config.getStringValue("password") + ";");
+		url.append("user=" + Config.getStringValue("user") + ";");
+		url.append("password=" + Config.getStringValue("password") + ";");
 		DB_URL = url.toString();
 	}
 	
 	static {
 		datasource = new DataSource();
 		PoolProperties p = new PoolProperties();
-		
 		p.setDriverClassName(JDBC_DRIVER);
 		p.setUrl(DB_URL);
-        
-        
+		p.setValidationQuery("SELECT 1");
+		p.setMaxActive(1000);
+		p.setInitialSize(100);
+		datasource.setPoolProperties(p);
 	}
 	
 	private DBConnection() {
 		
+	}
+	
+	public static Connection getConnection() {
+		try {
+			con = datasource.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
 	}
 }
